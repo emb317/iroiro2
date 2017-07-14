ddc8264ce432dad31dfcb6c648c137391b3c2c77
suteaka78 <suteaka78@gmail.com>
2017-07-14 12:26:38 +0900

    改行コードチェックツール

---
+++ return_code_check/Program.py
@@ -0,0 +1,39 @@
+﻿# coding: utf-8
+
+import sys
+from System import *
+from System.IO import *
+
+def PrintFile(path):
+	fs = FileStream(path, FileMode.Open, FileAccess.Read)
+	
+	crlf_cnt = 0
+	
+	for i in range(max(fs.Length-2, 500)):
+		if fs.ReadByte()==13:
+			if fs.ReadByte()==10:
+				crlf_cnt += 1
+				if crlf_cnt >= 10:
+					break
+	
+	# 閉じる
+	fs.Close()
+	
+	if crlf_cnt==0:
+		print(str(crlf_cnt) + " : " + path)
+
+def CheckDir(path):
+	try:
+		for dir_path in Directory.GetDirectories(path):
+			CheckDir(dir_path)
+	except:
+		pass
+
+	try:
+		for file_path in Directory.GetFiles(path):
+			PrintFile(file_path)
+	except:
+		pass
+
+for i in range(1, len(sys.argv)):
+	CheckDir(sys.argv[i])
